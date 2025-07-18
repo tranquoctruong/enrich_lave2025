eb0259f2dd16b5563c6b67b4061e7ea08ab06051
quoc truong <tranquoctruong195@gmail.com>
2025-07-18 20:23:29 +0700

    add swagger

--- IdentityService.API/Program.cs
+++ IdentityService.API/Program.cs
@@ -58,11 +58,8 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
